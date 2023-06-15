85814236a584d578bf182a90d956e8a72013d0d2
Bishop Varnell <bishop@loupe.team>
2023-06-15 12:07:20 -0600

    add searching for atn command and states

--- app/js/atn.js
+++ app/js/atn.js
@@ -1,7 +1,9 @@
 machine.writeVariable("AtnHmi:refreshState", 1);
+var systemState;
+
 machine.readVariable("AtnHmi:systemState", function () {
 
-    var systemState = JSON.parse(machine["AtnHmi:systemState"])
+    systemState = JSON.parse(machine["AtnHmi:systemState"])
     systemState.commands = systemState.commands.sort();
     systemState.States = systemState.States.sort();
 
@@ -14,6 +16,35 @@ machine.readVariable("AtnHmi:systemState", function () {
     $('#atnCommand').html(commands)
     $('#atnState').html(states)
 });
+
+function filterCommand(e){
+    var filterCommands;
+
+    filterCommands = (e.value == '') ? systemState.commands : filterItems(systemState.commands, e.value);
+
+    let commands = filterCommands.map(function (t) {
+        return `<li class="list-group-item" onclick="selectCommand('${t}')"> ${t} </li>`
+    })
+
+    $('#atnCommand').html(commands)
+    $('#atnCommandDropDown').addClass("open")
+}
+
+function filterState(e){
+    var filterStates;
+    filterStates = (e.value == '') ? systemState.States : filterItems(systemState.States, e.value);
+
+    let states = filterStates.map(function (t) {
+        return `<li class="list-group-item" onclick="selectState('${t}')"> ${t} </li>`
+    })
+
+    $('#atnState').html(states)
+    $('#atnStateDropDown').addClass("open")
+}
+
+function filterItems(arr, query) {
+    return arr.filter((el) => el.toLowerCase().includes(query.toLowerCase()));
+}
 
 function selectCommand(which) {
     machine.writeVariable("AtnHmi:AtnPLCOpenCall[0].Command", which);
